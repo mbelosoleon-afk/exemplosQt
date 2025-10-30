8cfa41187fc71c7020d3d86942cfc468e05dbf6d
dam <mbelosoleon@danielcastelao.org>
2025-10-30 09:37:44 +0100

    .

--- exemploBox.py
+++ exemploBox.py
@@ -6,7 +6,10 @@ class exemploBox (QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Exemplo con Box")
+
         caixaH = QHBoxLayout()
+
+        #Bloque 1
         caixaV1 = QVBoxLayout()
         caixaV1.addWidget(CaixaCor.CaixaCor("blue"))
         caixaV1.addWidget(CaixaCor.CaixaCor("green"))
@@ -15,15 +18,18 @@ class exemploBox (QMainWindow):
 
         caixaH.addWidget(CaixaCor.CaixaCor("yellow"))
 
-
+        #Bloque 2
         caixaV2 = QVBoxLayout()
         caixaV2.addWidget(CaixaCor.CaixaCor("pink"))
         caixaV2.addWidget(CaixaCor.CaixaCor("grey"))
         caixaH.addLayout(caixaV2)
+
+        #Creamos una referencia a un widget y le damos como layout a "CaixaH"
+        #De esa manera podremos observar los bloques coloreados
         container = QWidget()
         container.setLayout(caixaH)
         self.setCentralWidget(container)
-
+        #Muestra la ventana
         self.show()
 
 if __name__ == "__main__":
--- exemploGrid.py
+++ exemploGrid.py
@@ -8,19 +8,24 @@ class ExemploBox (QMainWindow):
         super().__init__()
         self.setWindowTitle("Exemplo con QGridLayout")
 
-        maia = QGridLayout()
-        maia.addWidget(CaixaCor.CaixaCor("red"))
+        maia = QGridLayout() #Crear una malla (Grid)
+        maia.addWidget(CaixaCor.CaixaCor("red")) #Le damos un color al primer objeto
+
+        #El resto de objetos + su forma de estructurarse:
+        #(widget,fila,columna,filas_ocupadas,columnas_ocupadas)
         maia.addWidget(CaixaCor.CaixaCor("blue"), 0,1,1,2)
         maia.addWidget(CaixaCor.CaixaCor("green"),1,0,2,1)
         maia.addWidget(CaixaCor.CaixaCor("pink"),1,1,1,2)
         maia.addWidget(CaixaCor.CaixaCor("orange"),2,1,1,1)
         maia.addWidget(CaixaCor.CaixaCor("yellow"),2,2,1,1)
 
-
+        #Contenedor que recoge todos los objetos anteriores
         container = QWidget()
+        #Los añade a container
         container.setLayout(maia)
+        #Añade container al widgertCentral, para que se vea por pantalla
         self.setCentralWidget(container)
-
+        #Muestra la ventana
         self.show()
 
 if __name__ == "__main__":
