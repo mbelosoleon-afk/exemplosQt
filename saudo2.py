f4e39c42a023ec458c50a906aa10273bcf2672b1
dam <mbelosoleon@danielcastelao.org>
2025-11-13 11:33:46 +0100

    .

--- saudo2.py
+++ saudo2.py
@@ -1,40 +1,59 @@
 import sys
+from  PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import (QApplication, QMainWindow, QPushButton, QLabel, QVBoxLayout, QWidget)
 
-from PyQt6.QtWidgets import QMainWindow, QVBoxLayout, QApplication, QLabel, QPushButton, QWidget
+"""
+Muestra una segunda ventana
+"""
+class Ventana2(QMainWindow):
 
+    """
+    Permite el cambio de escena
+    """
+    def on_cambio(self):
+        if self.ventana_padre is not None:
+            #self.hide() # Oculta la ventana actual
+            self.primera_ventana = self.ventana_padre # Crea una referencia a la otra ventana
+            self.primera_ventana.show() # Muestra la nueva ventana
+            self.destroy() # elimina esta ventana para ahorrar espacio en memoria al tener dos ventanas abiertas
+            print("Ventana destruida")
 
-class SegundaFiestra (QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("Mi segunda ventana")
-        self.setMinimumSize(400, 300)
+    """
+    Define la estructura de la ventana
+    """
+    def __init__(self , ventana_padre):
+        super().__init__() # Llama al super
 
-        caixa = QVBoxLayout()
+        self.primera_ventana = None # Crea una referencia para usarla en "on_cambio"
+        self.ventana_padre = ventana_padre # Referencia a la ventana padre
 
-        self.lblEtiqueta = QLabel("Ola")
-        self.lblEtiqueta.setText("Esta es la segunda ventana")
+        self.setWindowTitle("A miña segunda fiestra con QT") # Titulo
+        self.setMinimumSize(500,300) # Tamaño de la ventana
 
-        btnSaudo = QPushButton("Abrir la primera ventana")
-        btnSaudo.clicked.connect(self.abrirventanapadre)
+        self.label = QLabel("Hola") # Lo primero que se muestra si no se modifica
+        self.label.setText("Pepsi cola") # Se modifica lo anterior y se cambia
+        self.label.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignHCenter) # Centrar el texto
 
-        caixa.addWidget(self.lblEtiqueta)
-        caixa.addWidget(btnSaudo)
+        # Botón para regresar a la ventana anterior
+        boton_cambio_ventana  = QPushButton("Anterior")
+        boton_cambio_ventana.clicked.connect(self.on_cambio) # Conectarse a la función que permitirá el cambio de escena
 
-        container = QWidget()
-        container.setLayout(caixa)
+        cajaV = QVBoxLayout() # Almacenar
 
+        cajaV.addWidget(self.label) # Añade el el label a la ventana
+        cajaV.addWidget(boton_cambio_ventana)
+
+        container = QWidget() # Almacenar
+
+        container.setLayout(cajaV)
         self.setCentralWidget(container)
 
-        self.show()
+        self.show() # Muestra la ventana actual
 
-    def abrirventanapadre(self):
-        self.hide()
-        from saudoQT import NosaPrimeiraFiestra
-        self.vp = NosaPrimeiraFiestra()
-        self.vp.show()
-
-
+"""
+Ejecuta todo el código
+"""
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    ventana = SegundaFiestra()
-    sys.exit(app.exec())
+    aplicacion = QApplication(sys.argv)
+    fiestra = Ventana2(None) # Es none porque no tiene ventana padre, se anida ella
+    aplicacion.exec()
--- saudoQT.py
+++ saudoQT.py
@@ -1,95 +1,100 @@
 import sys
-from distutils.core import setup_keywords
-from os import setegid
+from time import sleep
 
+import Ventana2
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import (QApplication, QMainWindow, QPushButton, QLabel, QLineEdit, QBoxLayout, QVBoxLayout,
-                             QWidget, QCheckBox, QHBoxLayout)
+from PyQt6.QtWidgets import (QApplication, QMainWindow, QPushButton, QLabel, QLineEdit, QVBoxLayout, QWidget, QCheckBox,
+                             QHBoxLayout)
+
+"""
+Ventana 1 del programa
+"""
 
 
+class Ventana1(QMainWindow):
+    """
+    Modifica el texto principal y vacía el valor de la barra donde se introducen datos
+    """
 
-class NosaPrimeiraFiestra (QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("A miña primeira fiestra con QT")
-        self.setMinimumSize(500,300)
-        caixaV = QVBoxLayout()
-
-        self.nomeOculto = ""
-
-        self.lblEtiqueta = QLabel("OLA A TODOS!")
-        self.lblEtiqueta.setText("Ola a todos e todas")
-
-        self.txtSaudo = QLineEdit()
-        self.txtSaudo.setPlaceholderText("Introduce tu nombre")
-        self.txtSaudo.returnPressed.connect(self.on_btnSaudo_clicked)
-        self.txtSaudo.textChanged.connect(self.on_btnSaudo_textChanged)
-
-        btnSaudo = QPushButton ("Saúdo")
-        btnSaudo.clicked.connect(self.on_btnSaudo_clicked)
-
-        """Mudamos btnMaiusculas por chkMaiusculas
-        self.btnMaiusculas = QPushButton("Maiusculas")
-        self.btnMaiusculas.setCheckable(True)
-        self.btnMaiusculas.setChecked(True)
-        self.btnMaiusculas.toggled.connect(self.on_btnMaiusculas_toggled)
-        self.maiusculas = True
-        """
-        caixaH = QHBoxLayout()
-
-        self.chkMaiusculas = QCheckBox("Maiusculas")
-        self.chkMaiusculas.setChecked(True)
-        self.chkMaiusculas.toggled.connect(self.on_chkMaiusculas_toggled)
-        self.maiusculas = True
-
-        self.chkOculto = QCheckBox("Oculto")
-        caixaH.addWidget(self.chkMaiusculas)
-        caixaH.addWidget(self.chkOculto)
-        self.chkOculto.toggled.connect(self.on_chkOculto_toggled)
-
-        btn2 = QPushButton ("Abrir la segunda ventana")
-        btn2.clicked.connect(self.abrirVentana2)
-        caixaV.addWidget(btn2)
-
-        caixaV.addWidget(self.lblEtiqueta)
-        caixaV.addWidget(self.txtSaudo)
-        caixaV.addWidget(btnSaudo)
-        #caixaV.addWidget(self.btnMaiusculas)
-        #caixaV.addWidget(self.chkMaiusculas)
-        caixaV.addLayout(caixaH)
-
-        container = QWidget()
-        container.setLayout(caixaV)
-
-        self.setCentralWidget(container)
-        self.show()
-
-    def on_btnSaudo_clicked (self):
+    def on_btnSaudo_clicked(self):
+        nome = self.txtSaudo.text()
         if self.chkOculto.isChecked():
-            nome = self.nomeOculto
-            nome.strip()
-        if len(nome)!=0 :
-            self.txtSaudo.clear()
-            #self.txtSaudo.setText("")
-            self.lblEtiqueta.setText("Hola " + nome + " encantando de conocerte")
-        else:
-            self.lblEtiqueta.setText("Introduce tu nombre para recibir un saludo personalizado")
-        self.on_chkMaiusculas_toggled()
-    def abrirVentana2(self):
-        self.hide()
-        from saudo2 import SegundaFiestra
-        self.a =  SegundaFiestra()
-        self.a.show
+            nome = self.nomeOculto  # crea una variable nome con formato de texto
+        if self.txtSaudo.text() != "":  # si es nulo el valor introducido
+            if self.txtSaudo.text().isdigit():  # o es un número
+                self.txtSaudo.clear()  # muestra un error
+                self.lblEtiqueta.setText("Nome non permitido")
+            else:
+                self.txtSaudo.clear()  # Si no muestra un texto con el nombre añadido
+                # self.lblEtiqueta.setText("Ola "+ nome + " encantado/a de coñecerte")
+                self.lblEtiqueta.setText(nome)
+                self.nomeOculto = ''
+                self.txtSaudo.setText('')
 
-    def on_chkMaiusculas_toggled (self):
-        if self.chkMaiusculas.isChecked():
-            self.lblEtiqueta.setText(self.lblEtiqueta.text().upper())
-            self.maiusculas = True
-        else:
-            self.lblEtiqueta.setText(self.lblEtiqueta.text().lower())
-            self.maiusculas = False
+    """
+    Permite cambiar de ventana
+    """
 
-    def on_chkOculto_toggled(self):
+    def cambio_ventana(self):
+        if self.ventana_secundaria is None:  # si la referencia a la otra ventana  es nula
+            self.ventana_secundaria = Ventana2.Ventana2(self)  # crea una referencia a la otra ventana
+        self.hide()  # se oculta la ventana 1
+        self.ventana_secundaria.show()  # muestra la ventana 2
+
+    """
+    MUDADO A on_chk_mayusculas_toogled
+    Se ejecuta cuando se presiona el botón mayúsculas
+    Transforma la etiqueta a mayúsculas
+
+    def boton_mayusculas_presionado(self):
+        if self.btnMayusculas.isChecked(): # Si está seleccionado el botón de mayúsculas
+            self.lblEtiqueta.setText(self.lblEtiqueta.text().upper()) # Transforma el texto en mayúscula
+            self.mayusculas = True
+        else:
+            self.lblEtiqueta.setText(self.lblEtiqueta.text().lower()) # Transforma el texto en minúscula
+            self.mayusculas = False
+    """
+
+    """
+    De pendiendo si es o no mayúscula permite escribir SOLO en mayúscula o SOLO en minúscula
+    """
+
+    def on_btnSaudo_text_change(self):
+        if self.mayusculas:
+            self.txtSaudo.setText(self.txtSaudo.text().upper())
+        else:
+            self.txtSaudo.setText(self.txtSaudo.text().lower())
+
+        nome = self.txtSaudo.text()  # Instancia del texto
+        if self.chkOculto.isChecked():
+            for i, caracteres in enumerate(nome):  # Recorre cada char de la palabra
+                if caracteres != "*":  # Cuando uno de los chars es diferente a "*"
+                    if len(self.nomeOculto) == i:  # Si coincide lon el tamaño del nombre oculto
+                        self.nomeOculto = self.nomeOculto + caracteres  # Le da al nombre oculto el character del texto
+                        break
+                    else:
+                        self.nomeOculto = self.nomeOculto[:i] + caracteres + self.nomeOculto[i + 1:]
+            self.txtSaudo.setText('*' * len(self.nomeOculto))
+
+    """
+    Es lo mismo que el método mudado pero ahora en vez de un botón normal es un check
+    """
+
+    def on_chk_mayusculas_toogled(self):
+        if self.chkMaiusculas.isChecked():  # Si está seleccionado el botón de mayúsculas
+            self.lblEtiqueta.setText(self.lblEtiqueta.text().upper())  # Transforma el texto en mayúscula
+            self.txtSaudo.setText(self.txtSaudo.text().upper())
+            self.mayusculas = True
+        else:
+            self.lblEtiqueta.setText(self.lblEtiqueta.text().lower())  # Transforma el texto en minúscula
+            self.txtSaudo.setText(self.txtSaudo.text().lower())
+            self.mayusculas = False
+
+    """
+    Para modificar el texto cuando se selecciones el checked ocultar
+    """
+
+    def on_ocultar_toggled(self):
         if self.chkOculto.isChecked():
             self.nomeOculto = self.txtSaudo.text()
             self.txtSaudo.setText('*' * len(self.nomeOculto))
@@ -97,22 +102,99 @@ class NosaPrimeiraFiestra (QMainWindow):
             self.txtSaudo.setText(self.nomeOculto)
             self.nomeOculto = ''
 
-    def on_btnSaudo_textChanged(self):
-        if self.maiusculas:
-            self.txtSaudo.setText(self.txtSaudo.text().upper())
-        else:
-            self.txtSaudo.setText(self.txtSaudo.text().lower())
-        nome = self.txtSaudo.text()
-        if self.chkOculto.isChecked():
-            for i, caracter in enumerate(nome):
-                if caracter != '*':
-                    if len(self.nomeOculto) == i:
-                        self.nomeOculto = self.nomeOculto + caracter
-                        break
-                    else:
-                        self.nomeOculto = self.nomeOculto [:i] + caracter + self.nomeOculto[i:]
-            self.txtSaudo.setText('*' * len(self.nomeOculto))
+    """
+    Define la estructura de la ventana
+    """
+
+    def __init__(self):
+        super().__init__()
+
+        # Referencias
+        self.nomeOculto = ""
+        self.mensaje_guardado = None
+        self.ventana_secundaria = None  # Referencia para usar en "cambio_ventana"
+
+        # Configuración de tamaño y título de esta ventana
+        self.setWindowTitle("Ventana 1")  # Título de la ventana
+        self.setMinimumSize(500, 300)  # Tamaño de la ventana
+
+        # Configuración del label inicial
+        self.lblEtiqueta = QLabel("OLA A TODOS!")  # Primer label, cambia cuando este se modifica
+        fonte = self.lblEtiqueta.font()  # Administrar la fuente de letra
+        fonte.setPointSize(30)  # Tamaño de la letra
+        self.lblEtiqueta.setFont(fonte)  # Proporciona a self ese tamaño de letra
+        self.lblEtiqueta.setAlignment(
+            Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignHCenter)  # Ajustar el texto, centrarlo
+        self.lblEtiqueta.setText("OLA A TODOS E TODAS!")  # Muestra un texto
+
+        # Metodo para la introducción de texto por una barra
+        self.txtSaudo = QLineEdit()
+
+        # Configuración de la barra de introducción de datos
+        self.txtSaudo.setPlaceholderText("Introduce un texto")  # Texto que muestra la barra de introducción de texto
+        self.txtSaudo.returnPressed.connect(self.on_btnSaudo_clicked)
+        self.txtSaudo.textChanged.connect(self.on_btnSaudo_text_change)
+
+        # Configuración del botón de saudo
+        btnSaudo = QPushButton("Saudo")  # Crea un botón con ese nombre
+        btnSaudo.clicked.connect(self.on_btnSaudo_clicked)  # Crea una conexión con la función "on_btnSaudo_clicked"
+
+        # Configuración del botón de cambiar ventana
+        btnCambio = QPushButton("Cambiar")  # Botón para cambiar de escena
+        btnCambio.clicked.connect(self.cambio_ventana)  # Crea una conexión con la función "cambio_ventana"
+
+        """
+        MUDAMOS ESTE BOTON POR chkMaiusculas
+        # Configuración del botón de mayúsculas
+        self.btnMayusculas = QPushButton("Mayúsculas")
+        self.btnMayusculas.setCheckable(True) # le añadimos un self para verificar su estado en otros métodos
+        self.btnMayusculas.setChecked(True)
+        self.btnMayusculas.toggled.connect(self.boton_mayusculas_presionado)
+        self.mayusculas = True
+        """
+
+        # Para ordenar los bloques de manera horizontal
+        caixaH = QHBoxLayout()
+
+        # Configuración del check de mayúsculas
+        # En vez de ser un botón como el anterior ( el mudado ) es un recuadro de check
+        self.chkMaiusculas = QCheckBox("MAYÚSCULAS")
+        self.chkMaiusculas.setChecked(True)  # Aparece de principio seleccionado
+        self.chkMaiusculas.toggled.connect(self.on_chk_mayusculas_toogled)
+        self.mayusculas = True
+
+        self.chkOculto = QCheckBox("Ocultar")
+        self.chkOculto.setChecked(False)
+        self.chkOculto.toggled.connect(self.on_ocultar_toggled)
+
+        # Añade al contenedor horizontal
+        caixaH.addWidget(self.chkMaiusculas)
+        caixaH.addWidget(self.chkOculto)
+
+        # Almacenar el contenido de botones, labels , etc
+        caixaV = QVBoxLayout()
+
+        # Añade a la ventana los elementos anteriores
+        caixaV.addWidget(self.lblEtiqueta)
+        caixaV.addWidget(self.txtSaudo)
+        caixaV.addWidget(btnSaudo)
+        caixaV.addWidget(btnCambio)
+        # caixaV.addWidget(self.btnMayusculas) # Metodo mudado
+        caixaV.addLayout(caixaH)
+
+        # Almacena todos los caixaV
+        container = QWidget()
+        container.setLayout(caixaV)
+        self.setCentralWidget(container)
+
+        # Muestra la ventana
+        self.show()
+
+
+"""
+Ejecuta la ventana
+"""
 if __name__ == "__main__":
-    aplicacion = QApplication(sys.argv)
-    fiestra = NosaPrimeiraFiestra()
-    aplicacion.exec()
+    aplication = QApplication(sys.argv)
+    fiestra = Ventana1()
+    aplication.exec()
